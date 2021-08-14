333d78a6cfe362e4a6a5f1e4a7602799f75fdf7d
mendoanjoe <mendoanjoe@gmail.com>
2021-08-14 09:41:23 +0700

    add caching response using redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module, CacheInterceptor  } from '@nestjs/common';
+import * as redisStore from 'cache-manager-redis-store';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
+  imports: [
+    CacheModule.register({
+      store: redisStore,
+      host: 'redis.cache',
+      port: 6379,
+    }),
+    DatabaseModule,
+    ArticlesModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
-  imports: [DatabaseModule, ArticlesModule],
+  providers: [
+      {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+    AppService
+  ],
 })
 export class AppModule {}
